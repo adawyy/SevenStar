42624465c7770588cba923be6b91e1023d507451
GoldenRazor <goldenrazor@163.com>
2017-03-09 17:56:44 +0800

    添加所有成员的学习空间

---
+++ src/main/java/com/learnAT/martin/page/公用页.java
@@ -0,0 +1,57 @@
+package com.learnAT.martin.page;
+
+import com.mte.base.MteSenseBasePage;
+import com.mte.base.MteSenseCore;
+import com.mte.base.MteSenseLocator;
+import org.openqa.selenium.By;
+
+/**
+ * 创建人 Jackson
+ * 时间 2017/2/18
+ */
+
+public class 公用页 extends MteSenseBasePage {
+
+	MteSenseLocator locator = new MteSenseLocator("object/公用.yaml");
+
+	private String pageName = this.getClass().getSimpleName();
+
+	public 公用页(MteSenseCore asCore) {
+		super(asCore);
+		// TODO Auto-generated constructor stub
+	}
+
+	public By 用户名输入框() {
+		return locator.getLocator("用户名");
+	}
+
+    public By 密码输入框(){
+		return locator.getLocator("密码");
+    }
+
+    public By 登录按钮(){
+    	return locator.getLocator("登录按钮");
+    }
+
+	public By 快速进入按钮(){
+		return locator.getLocator("快速进入");
+	}
+
+	public By 同意按钮(){
+		return locator.getLocator("同意按钮");
+	}
+
+	public By 不同意按钮(){
+		return locator.getLocator("不同意按钮");
+	}
+
+	public By 右下弹框关闭按钮(){
+		return locator.getLocator("右下弹框关闭按钮");
+	}
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/learnAT/stefan/data/Datamgr.java
@@ -0,0 +1,140 @@
+package com.learnAT.stefan.data;
+
+import com.mte.util.datamgr.GetData;
+
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+/**
+ * 创建人 Jackson
+ * 时间 2017/2/18
+ */
+
+public class Datamgr {
+
+	String ExeclInput;
+	String ExcelVerify;
+
+	public void 设置数据文件(String xls){
+		this.ExeclInput = xls;
+	}
+
+	public String 获取数据文件(){
+		return this.ExeclInput;
+	}
+
+	public ArrayList<Hashtable<String,String>> 获取登录信息(String 表名){
+		ArrayList<Hashtable<String, String>> al;
+		al = new ArrayList<Hashtable<String, String>>();
+		try {
+			al = GetData.getGroupData(this.ExeclInput, "登录信息", 表名);
+			//		ht = al.get(0);
+			if (al.size() > 0) {
+
+			} else {
+				System.out.println("no Data found from excel!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return al;
+	}
+
+	public ArrayList<Hashtable<String,String>> 获取赔率设置(String 表名){
+		ArrayList<Hashtable<String, String>> al;
+		al = new ArrayList<Hashtable<String, String>>();
+		try {
+			al = GetData.getGroupData(this.ExeclInput, "赔率设置", 表名);
+			//		ht = al.get(0);
+			if (al.size() > 0) {
+
+			} else {
+				System.out.println("no Data found from excel!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return al;
+	}
+
+	public ArrayList<Hashtable<String,String>> 获取会员资料(String 表名){
+		ArrayList<Hashtable<String, String>> al;
+		al = new ArrayList<Hashtable<String, String>>();
+		try {
+			al = GetData.getGroupData(this.ExeclInput, "会员资料", 表名);
+			//		ht = al.get(0);
+			if (al.size() > 0) {
+
+			} else {
+				System.out.println("no Data found from excel!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return al;
+	}
+
+	public ArrayList<Hashtable<String,String>> 获取各级设置信息(String 表名){
+		ArrayList<Hashtable<String, String>> al;
+		al = new ArrayList<Hashtable<String, String>>();
+		try {
+			al = GetData.getGroupData(this.ExeclInput, "各级设置", 表名);
+			//		ht = al.get(0);
+			if (al.size() > 0) {
+
+			} else {
+				System.out.println("no Data found from excel!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return al;
+	}
+
+	public ArrayList<Hashtable<String,String>> 获取下注信息(String 表名){
+		ArrayList<Hashtable<String, String>> al;
+		al = new ArrayList<Hashtable<String, String>>();
+		try {
+			al = GetData.getGroupData(this.ExeclInput, "下注信息", 表名);
+			//		ht = al.get(0);
+			if (al.size() > 0) {
+
+			} else {
+				System.out.println("no Data found from excel!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return al;
+	}
+
+	public ArrayList<Hashtable<String,String>> 获取数据(String 页名,String 表名){
+		ArrayList<Hashtable<String, String>> al;
+		al = new ArrayList<Hashtable<String, String>>();
+		try {
+			al = GetData.getGroupData(this.ExeclInput, 页名, 表名);
+			//		ht = al.get(0);
+			if (al.size() > 0) {
+
+			} else {
+				System.out.println("no Data found from excel!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return al;
+	}
+
+
+	public static void main(String[] args) {
+		Datamgr dm = new Datamgr();
+		// TODO Auto-generated method stub
+
+	}
+
+}
